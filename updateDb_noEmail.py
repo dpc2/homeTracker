80ed2ed0507e8244952b6a6baa3fa878fafe9848
dpc2 <dpculler91@gmail.com>
2023-12-27 16:57:30 -0600

    Uploading pics from 12/26, new python script for updating db without sending email, db update

---
+++ updateDb_noEmail.py
@@ -0,0 +1,32 @@
+import sqlite3
+import datetime as dt
+from datetime import datetime, timedelta
+import yagmail
+
+
+conn = sqlite3.connect('database.db')
+plants = conn.execute('SELECT * FROM plants').fetchall()
+
+today = dt.datetime.now()
+temp = today.strftime('%Y-%m-%d')
+
+thirsty = ''
+email_list = "emailAddresses.txt"
+thirsty_boi = "/home/danny/scripts/plantTracker/venv/plantFlask/static/thirstyBoy.png"
+
+for item in plants:
+	lastWatered = dt.datetime.strptime(item[2], '%Y-%m-%d')
+	delta = int(item[3])
+	daysSince = today - lastWatered
+	daysMod = daysSince.days
+	name = item[1]
+	timeLeft = delta - daysMod
+
+	if daysMod >= delta:
+        	thirsty += str(item[1]) + """ bby says, "Please water me! It's been """ + str(daysMod) + " whole days!\"\n\n"
+
+	conn.execute('UPDATE plants SET remaining = ? WHERE name = ?', (timeLeft, name))
+
+conn.commit()
+conn.close()
+
